423b104e5e5ffbad2f963a0335a4b1216bbc31b6
Lucas Abreu <giovaneabreu@live.com>
2021-03-24 19:31:00 -0300

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
 			name: 'Red DiscordBot',
 			exec_mode: 'cluster',
 			instances: 2, // Or a number of instances
-			interpreter:  '/home/forge/redenv/bin/python',
-			script: 'redbot nehalem',
+			interpreter: '/home/forge/redenv/bin/python',
+			script: '/home/forge/redenv/bin/redbot nehalem',
 			args: 'start',
 			watch: true,
 		}
